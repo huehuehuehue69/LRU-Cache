8b298739bc2d0028b4afd3f28758bdfca7e35cd6
huehuehuehue69 <lakshyadeeppatel@gmail.com>
2023-08-03 00:09:57 +0530

    edge cases added

--- main.cpp
+++ main.cpp
@@ -5,40 +5,45 @@ using namespace std;
 int main()
 {
     int t;
-    cout<<"Enter No of TestCases."<<endl;
+    cout << "Enter No of TestCases." << endl;
     cin >> t;
     while (t--)
     {
         int capacity;
-        cout<<"Enter Capacity For Cache."<<endl;
+        cout << "Enter Capacity For Cache." << endl;
         cin >> capacity;
         LRUCache *cache = new LRUCache(capacity);
         int queries;
-        cout<<"Enter the number of Queries"<<endl;
+        cout << "Enter the number of Queries" << endl;
         cin >> queries;
         while (queries--)
         {
-            cout<<"Enter Query Type"<<endl;
-            cout<<"SET For Setting the Value."<<endl;
-            cout<<"GET for Getting the Value."<<endl;
+            cout << "Enter Query Type" << endl;
+            cout << "SET For Setting the Value." << endl;
+            cout << "GET for Getting the Value." << endl;
             string q;
             cin >> q;
             if (q == "SET")
             {
-                cout<<"Enter the key and Value."<<endl;
+                cout << "Enter the key and Value." << endl;
                 int key;
                 cin >> key;
                 int value;
                 cin >> value;
                 cache->SET(key, value);
-                cout<<"Value Setting Successfully"<<endl;
+                cout << "Value Setting Successfully" << endl;
             }
             else
             {
-                cout<<"Enter the Key."<<endl;
+                cout << "Enter the Key." << endl;
                 int key;
                 cin >> key;
-                cout<<"The Value of Cache is " << cache->GET(key) << endl;
+                if (cache->GET(key) == -1)
+                {
+                    cout << "NOT FOUND" << endl;
+                    return;
+                }
+                cout << "The Value of Cache is " << cache->GET(key) << endl;
             }
         }
         cout << endl;
